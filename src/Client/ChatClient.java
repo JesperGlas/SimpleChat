9b82ddfd800e4b05ce165a35ebadefbfa95661b2
JesperGlas <jesper.glas@gmail.com>
2020-02-12 13:34:10 +0100

    Added client hostIP promtp and ping

--- src/Client/ChatClient.java
+++ src/Client/ChatClient.java
@@ -3,23 +3,32 @@ package Client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Date;
 import java.util.Scanner;
 
 public class ChatClient {
 
-    private static final String host = "localhost";
-    private static final int portNumber = 4444;
+    private static String host = "localhost";
+    private static int portNumber = 4444;
+    private static boolean hostAccepted = false;
 
     private String userName;
     private String serverHost;
     private int serverPort;
-    private Scanner userInputScanner;
 
     public static void main(String[] args){
         String readName = null;
         Scanner scan = new Scanner(System.in);
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (hostAccepted == false) {
+            host = promptHostIP(scan);
+            hostAccepted = pingHost(host);
+        }
+
         System.out.println("Enter Username:");
         while(readName == null || readName.trim().equals("")) {
             readName = scan.nextLine();
@@ -32,8 +41,8 @@ public class ChatClient {
         client.startClient(bufferedReader);
     }
 
-    private ChatClient(String userName, String host, int portNumber){
-        this.userName = userName;
+    private ChatClient(String name, String host, int portNumber) {
+        this.userName = name;
         this.serverHost = host;
         this.serverPort = portNumber;
     }
@@ -49,7 +58,6 @@ public class ChatClient {
             Thread serverAccessThread = new Thread(serverThread);
             serverAccessThread.start();
             while(serverAccessThread.isAlive()) {
-                System.out.println("Busy wait?");
                 String line;
                 if((line = bufferedReader.readLine()) != null){
                     serverThread.addNextMessage(line);
@@ -61,5 +69,36 @@ public class ChatClient {
         } catch(InterruptedException ex){
             System.out.println("Interrupted");
         }
+    }
+
+    private static String promptHostIP(Scanner scan) {
+        String serverIP = "localhost";
+        String input = null;
+        System.out.println("Enter IP of Server (Default is localhost):");
+        if ((input = scan.nextLine()) != null) {
+            serverIP = input;
+        }
+        return serverIP;
+    }
+
+    private static Boolean pingHost(String hostIP) {
+        try {
+            InetAddress host = InetAddress.getByName(hostIP);
+            System.out.println("Sending ping to host: " + hostIP);
+            Date start = new Date();
+            if (host.isReachable(5000)) {
+                Date stop = new Date();
+                long timeToRespond = (stop.getTime() - start.getTime());
+                System.out.println("Host responded in " + timeToRespond + " ms");
+                return true;
+            } else {
+                System.out.println("Could not connect to host");
+            }
+        } catch (UnknownHostException e) {
+            System.out.println("Fatal Connection Error: Unknown Host");
+        } catch (IOException e) {
+            System.out.println("Fatal Connection Error: IOException");
+        }
+        return false;
     }
 }
